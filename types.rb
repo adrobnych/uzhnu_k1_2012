5b849df455d7a30fb69ee42a72b543543cfb3e0a
adrobnych <adrobnych@gmail.com>
2012-09-26 17:48:22 +0300

    types

---
+++ types.rb
@@ -0,0 +1,18 @@
+#Numbers
+x = -42
+y = 34000
+z = 45.67
+#Booleans
+x= true
+y = false
+#Strings
+s = "apple"
+ss = 'banana'
+#Symbols
+s = :apple
+#Arrays
+rating = ["apple", "banana"]
+#Hashes
+fruits = {:apple => 'red', :banana => 'yellow'}
+#Ranges
+r = (1..10)
